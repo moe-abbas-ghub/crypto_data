bcf9c8881ec3cf87698503adb3dd0bc116591190
Moe Abbas <yvu4eZ@protonmail.ch>
2022-11-12 16:24:40 +0100

    _

---
+++ CryptoData.py
@@ -0,0 +1,15 @@
+import pandas_datareader.data as Reader
+import datetime as Dt
+import os
+
+End = Dt.datetime.now()
+Start = Dt.date(End.year - 4, End.month, End.day)
+
+CryptoList = ['BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD']
+
+Crpyto_Df = Reader.get_data_yahoo(CryptoList, Start, End)['Adj Close']
+
+### We need to reset the index in order to have the date column included in the csv export
+Crpyto_Df = Crpyto_Df.reset_index()
+
+Crpyto_Df.to_csv(os.getcwd() + "/output_" + Dt.datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv', index = False)
