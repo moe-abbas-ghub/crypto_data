4842b0b453ae0b67ef9c1c02964ed98d615c7095
Moe Abbas <yvu4eZ@protonmail.ch>
2022-11-15 20:56:50 +0100

    _

--- CryptoData.py
+++ CryptoData.py
@@ -1,15 +1,11 @@
 #!//usr/local/bin/python3
-import pandas_datareader.data as Reader
-import datetime as Dt
+import pandas_datareader.data as reader
+import datetime as dt
 import os
 
-End = Dt.datetime.now()
-Start = Dt.date(End.year - 4, End.month, End.day)
-
+End = dt.datetime.now()
+Start = dt.date(End.year - 4, End.month, End.day)
 CryptoList = ['BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD']
-
-Crpyto_Df = Reader.get_data_yahoo(CryptoList, Start, End)['Adj Close']
-
+Crpyto_Df = reader.get_data_yahoo(CryptoList, Start, End)['Adj Close']
 Crpyto_Df = Crpyto_Df.reset_index()
-
-Crpyto_Df.to_csv(os.getcwd() + "/output_" + Dt.datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv', index = False)
+Crpyto_Df.to_csv(os.getcwd() + "/output_" + dt.datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv', index = False)
