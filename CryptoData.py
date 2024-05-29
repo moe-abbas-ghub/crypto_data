db8daaf068b61ecb67b7bae65190a7da9801f8a0
Moe Abbas <moe.abbas@oceansapart.com>
2024-05-29 12:25:58 +0200

    updated cryptodata.py with yfinance package

--- CryptoData.py
+++ CryptoData.py
@@ -1,11 +1,15 @@
-#!//usr/local/bin/python3
-import pandas_datareader.data as reader
+#!/usr/local/bin/python3
+import yfinance as yf
 import datetime as dt
 import os
 
 End = dt.datetime.now()
 Start = dt.date(End.year - 4, End.month, End.day)
 CryptoList = ['BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD']
-Crpyto_Df = reader.get_data_yahoo(CryptoList, Start, End)['Adj Close']
-Crpyto_Df = Crpyto_Df.reset_index()
-Crpyto_Df.to_csv(os.getcwd() + "/output_" + dt.datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv', index = False)
+
+# Download data
+Crypto_Df = yf.download(CryptoList, start=Start, end=End)['Adj Close']
+
+# Reset index and save to CSV
+Crypto_Df = Crypto_Df.reset_index()
+Crypto_Df.to_csv(os.getcwd() + "/output_" + dt.datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv', index=False)
